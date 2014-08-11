cc470201f1f65ca0857660d681991172222bd9b9
Zack Brown <z@charybrown.com>
2014-08-11 11:36:20 -0700

    break script out to external file; create script node instead of eval'ing

---
+++ famous-university-preview-adapter.js
@@ -0,0 +1,67 @@
+var _previousHTMLNode = undefined;
+
+window._handlerFunctions = {
+  css: function(cssString){
+    document.querySelector('head').innerHTML = "<style>html, body{margin: 0; padding: 0;} " + cssString + "</style>";
+  },
+  html: function(htmlString){
+    htmlString = "<div>" + htmlString + "</div>";
+    var injector = angular.injector(['ng','famous-university']);
+    injector.invoke(function($rootScope, $compile, $timeout){
+      //grab the container
+      var container = document.querySelector('#content-container');
+
+      //remove old compiled node
+      if(_previousHTMLNode !== undefined) container.removeChild(_previousHTMLNode);
+
+      //compile new node with Angular hooks
+      // debugger
+      try {
+        var node = $compile(htmlString)($rootScope);
+      }
+      catch (e) {
+        console.log(e)
+      }
+      //keep track of this new node as the "previous node" for next time
+      _previousHTMLNode = node[0];
+
+      //actually add the new compiled node to the document
+      container.appendChild(node[0]);
+
+      //make Angular digest in order to kick everything off
+      if(!$rootScope.$$phase)$rootScope.$apply();            
+    })
+  },
+  javascript: function(jsString){
+    var oldScript = document.querySelector('#injectable-script');
+    var body = document.querySelector('body');
+    console.log('src', oldScript)
+    if(oldScript !== null && oldScript !== undefined){
+      body.removeChild(oldScript);
+    }
+
+    var newScript = document.createElement('script');
+    newScript.setAttribute('id', 'injectable-script');
+    newScript.setAttribute('type', 'text/javascript');
+    newScript.innerHTML = jsString;
+
+    body.appendChild(newScript);
+  }
+}
+
+
+
+window.setFamousContent = function(content) {
+  angular.forEach(['javascript', 'css', 'html'], function(prop){
+    if(prop !== undefined) _handlerFunctions[prop](content[prop]);
+  });
+}
+
+// example call:
+
+
+// window.setFamousContent({
+//   html: "<fa-app ng-controller='TestCtrl' style='width: 320px; height: 568px;'><fa-modifier fa-translate='[0, random300(), 1]' fa-origin='[.5, .5]'><fa-surface fa-size='[20, 20]'><div class='red'>gurl</div></fa-surface></fa-modifier></fa-app>",
+//   javascript: "angular.module('famous-university').controller('TestCtrl', function($scope){ $scope.test = 'wooooo!'; $scope.random300 = function(){return Math.random() * 300}})",
+//   css: "div.red {background-color: red}"
+// });
